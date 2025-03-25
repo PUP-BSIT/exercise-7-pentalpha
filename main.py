81cb96b33ed5c7bab0121d7dd42a8aac8e1f89ac
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2025-03-25 21:14:45 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# TODO: (Frankie) Create a function that will take the product name, price
+# and quantity Store the result in an array in that order 
+    
+# TODO: (Angela) Create a loop to ask the user for input until all 
+# orders are complete
+
+# TODO: (Gabrielle) Ask for the customer name and the senior id 
+
+# TODO: (John Mark) Print the items and calculate the total
+
+# TODO: (John Matthew) Call all necessary functions and 
+# calculate the grand total
