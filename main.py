25241c2850b156e54b600de0f9a1d2af62205d8a
espinola_frankiejosh <joshmespinola@gmail.com>
2025-03-25 21:26:57 +0800

    add function for product input

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-# TODO: (Frankie) Create a function that will take the product name, price
-# and quantity Store the result in an array in that order 
+def get_product():
+    product_name = input("Enter product: ")
+
+    while True:
+        product_price = float(input("Product price: "))
+        if product_price <= 0:
+            print("Invalid input. Enter a positive number.") 
+        else:
+            break
     
+    while True:
+        product_quantity = int(input("Enter quantity: "))
+        if product_quantity <= 0:
+            print("Invalid input. Enter a positive number.") 
+        else:
+            break
+    
+    item =  [product_name, product_price, product_quantity]
+    return item
+
 # TODO: (Angela) Create a loop to ask the user for input until all 
 # orders are complete
 
